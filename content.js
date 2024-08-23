6eb8f3439301c0f0484d129c8b7f62ae0008535c
Mxthizu <mathieuvsf@hotmail.fr>
2024-08-24 00:05:59 +0200

    optim code

--- content.js
+++ content.js
@@ -1,88 +1,54 @@
 function calculateTRJ(odds) {
   let inverseSum = 0;
   odds.forEach((odd) => {
-    inverseSum += 1 / parseFloat(odd.replace(",", ".")); // Remplacer la virgule par un point pour les nombres décimaux
+    inverseSum += 1 / parseFloat(odd.replace(",", "."));
   });
   let trj = (1 / inverseSum) * 100;
   return trj.toFixed(2) + "%";
 }
 
-function getOddsForBetclic() {
-  const marketBlock = document.querySelector(
-    "sports-match-markets sports-markets-single-market .marketBox.is-table"
-  );
+function getOdds() {
+  let odds = [];
 
-  if (!marketBlock) {
-    console.log("MarketBlock non trouvé sur Betclic.");
-    return [];
+  if (window.location.hostname.includes("betclic.fr")) {
+    const marketBlock = document.querySelector(
+      "sports-match-markets sports-markets-single-market .marketBox.is-table"
+    );
+    if (marketBlock) {
+      const oddsElements = marketBlock.querySelectorAll(
+        ".btn.is-odd .btn_label:not(.is-top)"
+      );
+      oddsElements.forEach((element) => {
+        odds.push(element.textContent.trim());
+      });
+    }
+  } else if (window.location.hostname.includes("unibet.fr")) {
+    const marketBlock = document.querySelector(
+      "section#cps-marketcard.marketcard .marketcard-content"
+    );
+    if (marketBlock) {
+      const oddsElements = marketBlock.querySelectorAll(".oddbox-value span");
+      oddsElements.forEach((element) => {
+        odds.push(element.textContent.trim());
+      });
+    }
+  } else if (window.location.hostname.includes("enligne.parionssport.fdj.fr")) {
+    const marketBlock = document.querySelector(".psel-market-component");
+    if (marketBlock) {
+      const oddsElements = marketBlock.querySelectorAll(".psel-outcome__data");
+      oddsElements.forEach((element) => {
+        odds.push(element.textContent.trim());
+      });
+    }
   }
 
-  const oddsElements = marketBlock.querySelectorAll(
-    ".btn.is-odd .btn_label:not(.is-top)"
-  );
-  let odds = [];
-  oddsElements.forEach((element) => {
-    const oddValue = element.textContent.trim();
-    odds.push(oddValue);
-  });
-
-  console.log("Cotes trouvées sur Betclic :", odds);
-  return odds;
-}
-
-function getOddsForUnibet() {
-  const marketBlock = document.querySelector(
-    "section#cps-marketcard.marketcard .marketcard-content"
-  );
-
-  if (!marketBlock) {
-    console.log("MarketBlock non trouvé sur Unibet.");
-    return [];
-  }
-
-  const oddsElements = marketBlock.querySelectorAll(".oddbox-value span");
-  let odds = [];
-  oddsElements.forEach((element) => {
-    const oddValue = element.textContent.trim();
-    odds.push(oddValue);
-  });
-
-  console.log("Cotes trouvées sur Unibet :", odds);
-  return odds;
-}
-
-function getOddsForParionsSport() {
-  const marketBlock = document.querySelector(".psel-market-component");
-
-  if (!marketBlock) {
-    console.log("MarketBlock non trouvé sur ParionsSport.");
-    return [];
-  }
-
-  const oddsElements = marketBlock.querySelectorAll(".psel-outcome__data");
-  let odds = [];
-  oddsElements.forEach((element) => {
-    const oddValue = element.textContent.trim();
-    odds.push(oddValue);
-  });
-
-  console.log("Cotes trouvées sur ParionsSport :", odds);
   return odds;
 }
 
 function calculateAndDisplayTRJ() {
-  let odds = [];
-
-  if (window.location.hostname.includes("betclic.fr")) {
-    odds = getOddsForBetclic();
-  } else if (window.location.hostname.includes("unibet.fr")) {
-    odds = getOddsForUnibet();
-  } else if (window.location.hostname.includes("enligne.parionssport.fdj.fr")) {
-    odds = getOddsForParionsSport();
-  }
-
+  const odds = getOdds();
   if (odds.length === 3) {
-    let trj = calculateTRJ(odds);
+    const trj = calculateTRJ(odds);
     console.log("TRJ calculé : " + trj);
     return trj;
   } else {
@@ -95,33 +61,27 @@ function calculateAndDisplayTRJ() {
   }
 }
 
-// Utilisation d'un MutationObserver pour les sites qui chargent dynamiquement le contenu
 function observeDOMForDynamicSites() {
   const targetNode = document.body;
   const config = { childList: true, subtree: true };
 
   const callback = function (mutationsList, observer) {
-    for (let mutation of mutationsList) {
-      if (mutation.type === "childList") {
-        let marketBlock = null;
+    let marketBlock = null;
 
-        if (window.location.hostname.includes("unibet.fr")) {
-          marketBlock = document.querySelector(
-            "section#cps-marketcard.marketcard .marketcard-content"
-          );
-        } else if (
-          window.location.hostname.includes("enligne.parionssport.fdj.fr")
-        ) {
-          marketBlock = document.querySelector(".psel-market-component");
-        }
+    if (window.location.hostname.includes("unibet.fr")) {
+      marketBlock = document.querySelector(
+        "section#cps-marketcard.marketcard .marketcard-content"
+      );
+    } else if (
+      window.location.hostname.includes("enligne.parionssport.fdj.fr")
+    ) {
+      marketBlock = document.querySelector(".psel-market-component");
+    }
 
-        if (marketBlock) {
-          console.log("MarketBlock trouvé, arrêt de l'observation.");
-          observer.disconnect(); // Arrête d'observer après avoir trouvé le marketBlock
-          const trj = calculateAndDisplayTRJ();
-          return trj;
-        }
-      }
+    if (marketBlock) {
+      observer.disconnect();
+      const trj = calculateAndDisplayTRJ();
+      return trj;
     }
   };
 
